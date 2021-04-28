8ff5722041e03e73c33e9f2a1a9724ac2189a4e2
kosuke-17 <tamutamu17@icloud.com>
2021-04-28 18:27:26 +0900

    Providerでコンテキストを変更

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ let data = {title:'React-Context', message:'this is sample message.'}
 const SampleContext = React.createContext(data)
 
 class App extends Component {
+  newdata = {title:'新しいタイトル', message:'これは新しいメッセージです。'}
+
     render(){
     return <div>
     <h1 className="bg-primary text-white display-4">React</h1>
     <div className="container">
+    <Title />
+      <Message />
+      <hr />
+      <SampleContext.Provider value={this.newdata}>
+        <Title />
+        <Message />
+      </SampleContext.Provider>
+      <ht />
       <Title />
       <Message />
     </div>
