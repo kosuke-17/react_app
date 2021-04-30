7c442c8328e1d8622a443fa0c312b4cd85806d15
kosuke-17 <tamutamu17@icloud.com>
2021-04-30 13:49:37 +0900

    属性値の更新

--- src/App.js
+++ src/App.js
@@ -15,15 +15,24 @@ function CardMessage(props){
 }
 
 function App() {
-  const [msg] = useState("This is sample message!")
-  const [str] = useState("これはテスト")
+  const [msg, setMsg] = useState("This is sample message!")
+  const doAction = ()=>{
+    let res = window.prompt('type your name:')
+    setMsg("Hello, " +  res + "!!")
+  }
+  
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
         <AlertMessage message={msg} />
-        <CardMessage message={str} />
+        <CardMessage message={msg} />
+        <div className="text-center">
+          <button onClick={doAction} className="btn btn-primary">
+            Click me!
+          </button>
+        </div>
       </div>
     </div>
   )
