2b40bac346a47fb3032f15dcf95c12d1f77eed38
kosuke-17 <tamutamu17@icloud.com>
2021-04-30 14:31:19 +0900

    親と子コンポーネントで値をやりとりする

--- src/App.js
+++ src/App.js
@@ -3,35 +3,51 @@ import './App.css';
 
 
 function AlertMessage(props){
-  return <div className="alert alert-primary h5 text-primary">
-    {props.message}
-  </div>
+    const data = ["Hello!", "Welcome...", "Good-bye?"]
+
+    const actionAlert = () => {
+      const re = data[Math.floor(Math.random() * data.length)]
+      props.setAlert('message:"' + re + '".')
+    }
+    return <div className="alert alert-primary h5 text-primary">
+      <h5>{props.alert}</h5>
+      <button onClick={actionAlert} className="btn btn-primary">
+        Click me!
+      </button>
+    </div>
 }
 
 function CardMessage(props){
-  return <div className="card p-3 h5 border-primary text-center">
-    {props.message}
+  const [count, setCount] = useState(0)
+
+  const actionCard = () => {
+    setCount(count + 1)
+    props.setCard("card counter: " + count + " count")
+  }
+
+  return <div className="card p-3 border-dark text-center">
+    <h5>{props.card}</h5>
+    <button onClick={actionCard} className="btn btn-secondary">
+      Click me!
+    </button>
   </div>
 }
 
 function App() {
-  const [msg, setMsg] = useState("This is sample message!")
-  const doAction = ()=>{
-    let res = window.prompt('type your name:')
-    setMsg("Hello, " +  res + "!!")
-  }
+  const [alert, setAlert] = useState("this is alert message!")
+  const [card, setCard] = useState("This is card message!")
   
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage message={msg} />
-        <CardMessage message={msg} />
-        <div className="text-center">
-          <button onClick={doAction} className="btn btn-primary">
-            Click me!
-          </button>
+        <AlertMessage alert={alert} setAlert={setAlert} />
+        <CardMessage card={card} setCard={setCard} />
+        <hr />
+        <div className="text-right">
+          <p>{alert}</p>
+          <p>{card}</p>
         </div>
       </div>
     </div>
